1a3699df383dcfae014130d39d921f707dcabd2e
Guilherme Lima Facanha <limafacanhag@student.douglascollege.ca>
2019-03-26 15:29:54 -0700

    local db config

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,15 +1,15 @@
 <?php
 
 //Host Local
-//define("DB_HOST","localhost");
-//define("DB_USER","root");
-//define("DB_PASS","");
-//define("DB_NAME","LibraryDb");
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","LibraryDb");
 
 //Host Remote
-define("DB_HOST","reminder.ccqysqxekuuh.us-east-2.rds.amazonaws.com");
-define("DB_USER","adminuser");
-define("DB_PASS","admin123");
-define("DB_NAME","LibraryDb");
+//define("DB_HOST","reminder.ccqysqxekuuh.us-east-2.rds.amazonaws.com");
+//define("DB_USER","adminuser");
+//define("DB_PASS","admin123");
+//define("DB_NAME","LibraryDb");
 
 ?>
